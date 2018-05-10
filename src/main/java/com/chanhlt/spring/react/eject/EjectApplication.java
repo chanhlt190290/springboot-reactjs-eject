524a0de152052c2d4482f4ff996fef8bf6750ae2
unknown <trungchanh@OPEN.local>
2018-05-10 17:50:29 +0900

    initial commit

---
+++ src/main/java/com/chanhlt/spring/react/eject/EjectApplication.java
@@ -0,0 +1,12 @@
+package com.chanhlt.spring.react.eject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjectApplication.class, args);
+    }
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,3 @@
+function echo(){
+    alert('It works!');
+}
